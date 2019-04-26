208e8a46eb1827bccd5ac0752c65d9b0289151ab
Silas Hellinger <446547+Aevis@users.noreply.github.com>
2019-04-27 01:34:59 +0200

    Fix DSN properties and use different exit status

--- dbConnectionTest.php
+++ dbConnectionTest.php
@@ -1,11 +1,12 @@
 <?php
 try {
-    $db = new PDO('informix:host=localhost;service=9088;database=systables;server=awifxsvr;protocol=olsoctcp;CLIENT_LOCALE=en_us.utf8;DB_LOCALE=en_us.utf8;EnableScrollableCursors=1', 'informix', 'in4mix');
+    $db = new PDO('informix:host=localhost;service=9088;database=systables;server=informix;protocol=olsoctcp;CLIENT_LOCALE=en_us.utf8;DB_LOCALE=en_us.utf8;EnableScrollableCursors=1', 'informix', 'in4mix');
     $result=$db->query("select tabname from systables;");
     $row=$result->fetch(PDO::FETCH_NUM);
     print_r($row);
+    exit(0);
 
 } catch (PDOException $e) {
     print $e->getMessage();
-    die();
+    exit(1);
 }
