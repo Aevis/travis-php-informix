2cda7db8fc2f3da34c755842d9e8370ee21ee30b
Silas Hellinger <446547+Aevis@users.noreply.github.com>
2019-04-26 23:27:51 +0200

    Run a post-install shell script to create sbspace and the test DB

---
+++ post-install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+source /opt/ibm/scripts/informix_inf.env
+echo "Creating sbspace"
+touch "${INFORMIX_DATA_DIR}"/spaces/sbspace
+chmod 660 "${INFORMIX_DATA_DIR}"/spaces/sbspace
+onspaces -c -S sbspace -p "${INFORMIX_DATA_DIR}"/spaces/sbspace -o 0 -s 20000
+echo "Creating test database"
+echo "CREATE DATABASE test WITH BUFFERED LOG" | dbaccess > /dev/null 2>&1
+echo "Post install finished"
