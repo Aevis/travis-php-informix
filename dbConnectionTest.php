d29a2b4f40baf2a914ea0e86d6375d69a096a1c4
Silas Hellinger <446547+Aevis@users.noreply.github.com>
2019-04-27 02:13:34 +0200

    Remove unnecessary debugging commands

--- dbConnectionTest.php
+++ dbConnectionTest.php
@@ -1,6 +1,7 @@
 <?php
 try {
     $db = new PDO('informix:host=localhost;service=9088;database=test;server=informix;protocol=olsoctcp;CLIENT_LOCALE=en_us.utf8;DB_LOCALE=en_us.utf8;EnableScrollableCursors=1', 'informix', 'in4mix');
+    echo 'DB Connection successfully: ';
     $result=$db->query('select tabname from systables;');
     $row=$result->fetch(PDO::FETCH_NUM);
     print_r($row);
