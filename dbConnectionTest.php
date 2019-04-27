b785d6f33445c980e1c5f7907d15bf569f032764
Silas Hellinger <446547+Aevis@users.noreply.github.com>
2019-04-27 02:37:42 +0200

    Fix listing all databases

--- dbConnectionTest.php
+++ dbConnectionTest.php
@@ -2,7 +2,7 @@
 try {
     $db = new PDO('informix:host=localhost;service=9088;database=test;server=informix;protocol=olsoctcp;CLIENT_LOCALE=en_us.utf8;DB_LOCALE=en_us.utf8;EnableScrollableCursors=1', 'informix', 'in4mix');
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $result=$db->query('select * from sysdatabases;');
+    $result=$db->query('select name from sysmaster:sysdatabases;');
     $row=$result->fetch(PDO::FETCH_ASSOC);
     echo 'DB Connection successfull:';
     print_r($row);
