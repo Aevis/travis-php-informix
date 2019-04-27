e73b6de721af75ba86f104c7a19320ca869304c1
Silas Hellinger <446547+Aevis@users.noreply.github.com>
2019-04-27 02:18:32 +0200

    Query the test DB

--- dbConnectionTest.php
+++ dbConnectionTest.php
@@ -2,11 +2,10 @@
 try {
     $db = new PDO('informix:host=localhost;service=9088;database=test;server=informix;protocol=olsoctcp;CLIENT_LOCALE=en_us.utf8;DB_LOCALE=en_us.utf8;EnableScrollableCursors=1', 'informix', 'in4mix');
     echo 'DB Connection successfully: ';
-    $result=$db->query('select tabname from systables;');
+    $result=$db->query('select tabname from test;');
     $row=$result->fetch(PDO::FETCH_NUM);
     print_r($row);
     exit(0);
-
 } catch (PDOException $e) {
     print $e->getMessage().PHP_EOL;
     exit(1);
