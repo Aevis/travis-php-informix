d9e88af8f14c84e3d5e6a9cbc435c0c8e86dca8f
Silas Hellinger <446547+Aevis@users.noreply.github.com>
2019-04-27 02:42:42 +0200

    List all databases

--- dbConnectionTest.php
+++ dbConnectionTest.php
@@ -3,9 +3,9 @@ try {
     $db = new PDO('informix:host=localhost;service=9088;database=test;server=informix;protocol=olsoctcp;CLIENT_LOCALE=en_us.utf8;DB_LOCALE=en_us.utf8;EnableScrollableCursors=1', 'informix', 'in4mix');
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $result=$db->query('select name from sysmaster:sysdatabases;');
-    $row=$result->fetch(PDO::FETCH_ASSOC);
+    $dbnames=$result->fetchAll(PDO::FETCH_ASSOC);
     echo 'DB Connection successfull:';
-    print_r($row);
+    print_r($dbnames);
     exit(0);
 } catch (PDOException $e) {
     print $e->getMessage().PHP_EOL;
